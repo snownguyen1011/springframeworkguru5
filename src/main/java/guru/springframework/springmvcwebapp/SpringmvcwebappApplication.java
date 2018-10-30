8f51853ceb88bc3c296a46298c3ed6d5735f7e18
varmaraghu <varmaraghu.g@gmail.com>
2018-10-29 21:33:34 -0400

    first commit

---
+++ src/main/java/guru/springframework/springmvcwebapp/SpringmvcwebappApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.springmvcwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcwebappApplication.class, args);
+	}
+}
